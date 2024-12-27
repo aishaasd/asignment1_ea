20dd57f83b17f65755a228d80c9b4db5516fa7f5
aishaasd <aishaerbolat0404@gmail.com>
2024-12-27 13:35:49 +0500

    projectt\

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            Person person = new Person();
+            person.setName("Aisha");
+            person.setSurname("Yerbolat");
+            person.setAge(17);
+            person.setGender(false);
+            System.out.println(person.toString());
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private boolean gender;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public boolean getGender() {
+        return gender;
+    }
+        public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+    @Override
+    public String toString() {
+        String genderString=gender?"male":"female";
+        return "Hi, I am " + name + " " + surname + " a " + age+"-year-old "+genderString;
+    }
+}
